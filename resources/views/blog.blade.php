1271f002ca5e0b3f4cc72a549612c1911ee26245
Peterce3 <peterce3@gmail.com>
2025-05-14 12:21:09 +0700

    pertemuan 11

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tutorial Laravel - www.malasngoding.com</title>
+</head>
+<body>
+	<h3>www.malasngoding.com</h3>
+	<p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+	<p>Ini adalah view blog. ada di route blog.</p>
+    <?php
+    $nama = "Saul" ;
+    echo "Hallo saya " . $nama ;
+    ?>
+    <br>
+    {{"Hallo saya " . $nama}}
+    <br>
+    <?php
+        date_default_timezone_set('Asia/Jakarta');
+        echo date('d-m-Y H:i:s')
+    ?>
+</body>
+</html>
